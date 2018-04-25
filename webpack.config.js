558fc30c266003b63daedb8423a7e0770f0fcab8
Jason Ropp <jason.ropp@banno.com>
2018-04-25 12:04:02 -0400

    bleh

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const config = {
+	entry: './src/js/controls.js',
+	output: {
+    filename: 'bundle.js',
+    path: '/Users/jropp/Desktop/jason-ropp-js-iobp/src/dist'
+	},
+	mode: "development",
+};
+
+module.exports = config;
