7d5235cad17a8a0f5f9ef0c3f571c46a10ff9915
Jason Ropp <jason.ropp@banno.com>
2018-05-15 08:35:38 -0400

    some updates on links, added nav-bar.js

---
+++ src/nav-bar/nav-bar.js
@@ -0,0 +1,26 @@
+// // Copy of banno setup for accompanying .js file
+// goog.require('bannoWeb');
+
+// (() => {
+//   'use strict';
+
+//   /**
+//    * @polymer
+//    * @customElement
+//    * @extends {Polymer.Element}
+//    */
+//   window.NavBar = class extends Polymer.Element {
+//     static get is() {
+//       return 'nav-bar';
+//     }
+//     static get properties() {
+//       return {
+//         /** @type {?string} */
+//         accountId: String,
+//       };
+//     }
+
+
+//   };
+//   customElements.define(window.NavBar.is, window.NavBar);
+// })();
