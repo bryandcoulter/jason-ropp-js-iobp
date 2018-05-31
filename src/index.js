28eb13482de67bef03a1d89acd05caf4b7ff01f2
Jason Ropp <jason.ropp@banno.com>
2018-05-31 10:29:33 -0400

    less chaos, polymer not defined in bundle

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 // If I try to import this here it tries to resolve but cant interpret the polymer component
 // Thing learned: after using this as the entry point it doesn't go looking with other loaders
-// import './user-component/user-component.html';
+import '../node_modules/@banno/polymer/polymer.html';
+import './user-component/user-component.html';
+import './nav-bar/nav-bar.html';
--- webpack.config.js
+++ webpack.config.js
@@ -1,81 +1,74 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const WriteFilePlugin = require('write-file-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var path = require('path');
 
 module.exports = {
-	// Is this the correct entry point
-	entry: './src/index.js',
-	// Well at least output is working properly... I think
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js'
-  },
-  module: {
+	// Tell Webpack which file kicks off our app.
+	entry: path.resolve(__dirname, 'src/index.js'),
+	// Tell Weback to output our bundle to ./dist/bundle.js
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	// Tell Webpack which directories to look in to resolve import statements.
+	// Normally Webpack will look in node_modules by default but since we’re overriding
+	// the property we’ll need to tell it to look there in addition to the
+	// bower_components folder.
+	resolve: {
+		modules: [
+			path.resolve(__dirname, 'node_modules'),
+			path.resolve(__dirname, '@webcomponents')
+		]
+	},
+	// These rules tell Webpack how to process different module types.
+	// Remember, *everything* is a module in Webpack. That includes
+	// CSS, and (thanks to our loader) HTML.
+	module: {
 		rules: [
-			// My guess is I need this for any js files outside of the components, if I had them
-      {
-        test: /\.js$/,
-        use: 'babel-loader'
-			},
-			// I also don't have any css components right now.ssooooooo
-      // {
-      //   test: /\.css$/,
-      //   use: ['style-loader', 'css-loader']
-      // },
-      {
-        test: /\.html$/,
-        // Is this path style correct?
-        // include: path.resolve(__dirname, './*.html'),
-				// Is this format correct?
-				// Currently things are entering through index.ejs so I don't think I need to exclude index.html anymore
-        // exclude: [
-          // path.resolve(__dirname, './index.html'),
-          // Does this need a ./ at the beginning?
-          // path.resolve(__dirname, 'pages/*.html'),
-        // ],
-        use: [
-          // Chained loaders are applied last to first
-          // So how is html-loader included? Is that automatic while babel-loader is not?
-          { loader: 'babel-loader' },
-          { loader: 'polymer-webpack-loader' }
+			{
+				// If you see a file that ends in .html, send it to these loaders.
+				test: /\.html$/,
+				// This is an example of chained loaders in Webpack.
+				// Chained loaders run last to first. So it will run
+				// polymer-webpack-loader, and hand the output to
+				// babel-loader. This let's us transpile JS in our `<script>` elements.
+				use: [
+					{ loader: 'babel-loader' },
+					{ loader: 'polymer-webpack-loader' }
 				]
-				// These options caused an error for having options in the wrong place, maybe because I added a couple of loaders instead of just one
-        // options: {
-        //   name: '[name].[ext]',
-        //   outputPath: "/",
-        //   // Change later, for now I just want to be able to see output more clearly
-        //   minimize: false,
-        // }
-      }
-    ],
-  },
-  plugins: [
-		new CleanWebpackPlugin('dist'),
-		// It is now entering through index.ejs
-		// Is working properly when copywebpackplugin is being used
-		new HtmlWebPackPlugin({
-      template: path.resolve(__dirname, 'src/index.ejs'),
-      inject: false
-      // This means we have to have an index.ejs template file
-      // in our src directory. It will output a file we can use
-      // in the dev server, I guess.
-    }),
-		// Use this for webcomponent polyfills later
+			},
+			{
+				// If you see a file that ends in .js, just send it to the babel-loader.
+				test: /\.js$/,
+				use: 'babel-loader'
+				// Optionally exclude node_modules from transpilation except for polymer-webpack-loader:
+				// exclude: /node_modules\/(?!polymer-webpack-loader\/).*/
+			}
+		]
+	},
+	// Enable the Webpack dev server which will build, serve, and reload our
+	// project on changes.
+	devServer: {
+		contentBase: path.join(__dirname, 'dist'),
+		compress: true,
+		port: 1820
+	},
+	plugins: [
+		// This plugin will generate an index.html file for us that can be used
+		// by the Webpack dev server. We can give it a template file (written in EJS)
+		// and it will handle injecting our bundle for us.
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, 'src/index.ejs'),
+			inject: false
+		}),
+		// This plugin will copy files over for us without transforming them.
+		// That's important because the custom-elements-es5-adapter.js MUST
+		// remain in ES2015.
 		new CopyWebpackPlugin([
-      // { from: 'src/*/*.html', to: '[name].[ext]' },
-      // { from: 'src/index.html', to: '[name].[ext]' },
-      { from: 'node_modules/@banno/polymer/polymer.html', to: '[name].[ext]' },
-		]),
-		new WriteFilePlugin()
-    // // Does the same thing as copywebpackplugin but for webpack-dev-server
-    // new WriteFilePlugin(),
-  ],
-
-  // Anything that wp doesn't generate on its own gets served out of dist
-  devServer: {
-    contentBase: 'dist'
-  }
+			{ from: 'node_modules/@banno/polymer/polymer.html', to: '@banno/polymer/[name].[ext]' },
+			{
+				from: path.resolve(__dirname, 'node_modules/@webcomponents/webcomponentsjs/*.js'),
+				to: '@webcomponents/webcomponentsjs/[name].[ext]'
+			}])
+	]
 };
